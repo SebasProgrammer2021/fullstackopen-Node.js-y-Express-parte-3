54bfcde23a0fcc21ea8f15a7ef0e335cbf1111dc
Sebastián Londoño Valencia <sebastianl@titamedia.com>
2026-03-30 22:01:41 -0500

    se movio codigo quemado, datos quemados
    - implmetnacion de la base de datos para otras rutas y documentacion

--- index.js
+++ index.js
@@ -3,28 +3,10 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const Note = require('./models/note');
-
+const { notes } = require('./burned-data');
 app.use(express.json());
 app.use(cors())
 
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -40,10 +22,12 @@ const unknownEndpoint = (request, response) => {
 
 app.use(requestLogger)
 
+// ruta raíz
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+// obtener todas las notas
 app.get('/api/notes', (request, response) => {
   // Primera ruta usando MongoDB
   Note.find({})
@@ -56,25 +40,47 @@ app.get('/api/notes', (request, response) => {
 
 })
 
+// obtener una nota por id
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id) //el parametro siempre viene en string y debe convertirse a número.
-  console.log(id)
+  // const id = Number(request.params.id) //el parametro siempre viene en string y debe convertirse a número.
+  // console.log(id)
   // const note = notes.find(note => note.id === id)
-  const note = notes.find(note => {
-    return note.id === id
-  })
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).json({ message: "Nota no encontrada" }) //código 404 para not found
-  }
+
+  // FORMA 1 - usando el array de notas en memoria, datos quemados
+  // const note = notes.find(note => {
+  //   return note.id === id
+  // })
+  // if (note) {
+  //   response.json(note)
+  // } else {
+  //   response.status(404).json({ message: "Nota no encontrada" }) //código 404 para not found
+  // }
+
+  // FORMA 2 - usando MongoDB
+  Note.findById(request.params.id).
+    then(note => {
+      response.json(note)
+    })
+    .catch(error => {
+      console.error('Error consultando la nota por ID:', error.message)
+      response.status(500).json({ error: 'Error fetching note' })
+    })
 })
 
+// eliminar una nota por id
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  // const id = Number(request.params.id)
+  // notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end()
+  // response.status(204).end()
+  Note.deleteOne({ _id: request.params.id })
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.error('Error deleting note:', error.message)
+      response.status(500).json({ error: 'Error deleting note' })
+    })
 })
 
 const generateId = () => {
@@ -84,6 +90,7 @@ const generateId = () => {
   return maxId + 1
 }
 
+// crear una nueva nota
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -93,17 +100,33 @@ app.post('/api/notes', (request, response) => {
     return response.status(400).json({ error: 'Content missing' })
   }
 
-  const note = {
-    id: generateId(), // se genera un nuevo id para la nota utilizando la función generateId, que calcula el id máximo actual y le suma 1.
-    content: body.content, // se asigna el contenido de la nota a partir del cuerpo de la solicitud, que se espera que tenga una propiedad content.
-    important: Boolean(body.important) || false // se asigna la propiedad important a partir del cuerpo de la solicitud, convirtiendo su valor a booleano. Si no se proporciona un valor para important, se establece como false por defecto. esto permite que la nota tenga una propiedad important opcional, que puede ser true o false según lo que se envíe en la solicitud.
-  }
+  // FORMA 1 - usando el array de notas en memoria, datos quemados
+  // const note = {
+  //   id: generateId(), // se genera un nuevo id para la nota utilizando la función generateId, que calcula el id máximo actual y le suma 1.
+  //   content: body.content, // se asigna el contenido de la nota a partir del cuerpo de la solicitud, que se espera que tenga una propiedad content.
+  //   important: Boolean(body.important) || false // se asigna la propiedad important a partir del cuerpo de la solicitud, convirtiendo su valor a booleano. Si no se proporciona un valor para important, se establece como false por defecto. esto permite que la nota tenga una propiedad important opcional, que puede ser true o false según lo que se envíe en la solicitud.
+  // }
 
-  notes = notes.concat(note) // se agrega la nueva nota al array de notas existente utilizando el método concat, que devuelve un nuevo array con la nueva nota añadida al final. Esto es preferible a usar push, ya que concat no modifica el array original, lo que es una buena práctica en programación funcional.
+  // notes = notes.concat(note) // se agrega la nueva nota al array de notas existente utilizando el método concat, que devuelve un nuevo array con la nueva nota añadida al final. Esto es preferible a usar push, ya que concat no modifica el array original, lo que es una buena práctica en programación funcional.
+
+  //  response.json(note)
+
+  // FORMA 2 - usando MongoDB
+  const note = new Note({
+    content: body.content,
+    important: Boolean(body.important) || false
+  })
 
   console.log('note', note);
 
-  response.json(note)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => {
+      response.status(500).json({ error: error.message })
+    })
+
 })
 
 
